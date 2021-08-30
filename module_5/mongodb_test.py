346857b54c5e517f9941ee113c6dd436b3e77942
Sigfredo Monge <smonge@my365.bellevue.edu>
2021-08-30 14:37:19 -0600

    mongodb test

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,25 @@
+''' 
+    Title: mongodb_test.py
+    Author: Sigfredo Monge Villanueva
+    Date: 29 August 2021
+    Description: Test program for connecting to a MongoDB Atlas cluster
+'''
+
+""" import statements """
+from pymongo import MongoClient
+
+# MongoDB connection string 
+url = "mongodb+srv://admin:admin@cluster0.mbmui.mongodb.net/pytech"
+
+# connect to the MongoDB cluster 
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# show the connected collections 
+print("\n -- Pytech Collection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
